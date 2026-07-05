30b03c133cc8ce61ea19e1d3ed32f8e1708f1069
iipmnigeria <43499291+iipmnigeria@users.noreply.github.com>
2026-07-05 18:52:01 +0100

    Add payroll payment engine setup under settings

--- src/components/SettingsSuite.tsx
+++ src/components/SettingsSuite.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import SettingsGlobal from './SettingsGlobal';
 import PayrollRuleBuilder from './PayrollRuleBuilder';
+import PayrollPaymentEngineSettings from './PayrollPaymentEngineSettings';
 
 export default function SettingsSuite(props: any) {
   return (
     <div className="space-y-8">
       <SettingsGlobal {...props} />
+      <PayrollPaymentEngineSettings {...props} />
       <PayrollRuleBuilder {...props} />
     </div>
   );
