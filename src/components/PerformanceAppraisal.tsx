82a2234c2ce0317e0c3438044c625df1005fa7e8
iipmnigeria <43499291+iipmnigeria@users.noreply.github.com>
2026-07-07 20:14:49 +0100

    Add benefits engine to performance workspace

--- src/components/PerformanceAppraisal.tsx
+++ src/components/PerformanceAppraisal.tsx
@@ -1,3 +1,12 @@
+import React from 'react';
 import PerformanceManagementEngine from './PerformanceManagementEngine';
+import BenefitsManagementEngine from './BenefitsManagementEngine';
 
-export default PerformanceManagementEngine;
+export default function PerformanceAndBenefitsSuite(props: any) {
+  return (
+    <div className="space-y-10">
+      <PerformanceManagementEngine {...props} />
+      <BenefitsManagementEngine {...props} />
+    </div>
+  );
+}
